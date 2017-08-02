cc66d6b081eca8c7f03f860de40aeb7c49ecbbc2
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-08-02 16:29:05 +0200

    remove popup alerts

--- js/pages/visualizador-del-proceso/visualizador-del-proceso-controller.js
+++ js/pages/visualizador-del-proceso/visualizador-del-proceso-controller.js
@@ -1,45 +1,6 @@
 app.controller('VisualizadorDelProcesoController', function($scope, $rootScope, $http, $routeParams, config, ArtisterilIntervalService, $animate, $mdToast) {  
 
 
-    // Popup Alerts
-
-    $scope.popupAlertsData = {};
-    $scope.popupAlertsCount = 0;
-
-    $scope.getPopupAlertsData = function()
-    {
-        $http({
-            method  : 'GET',
-            url     : config.webservice.urls.data_get_popup_alerts
-         })
-        .then(function(response) {
-            $scope.popupAlertsData = {};
-            $scope.popupAlertsCount = 0;
-            for (i in response.data.get_popup_alertsResult) {
-                $scope.popupAlertsData[response.data.get_popup_alertsResult[i].Title + response.data.get_popup_alertsResult[i].Message] = response.data.get_popup_alertsResult[i];
-                $scope.popupAlertsCount += 1;
-            }
-        });
-    }
-    ArtisterilIntervalService.start($scope.getPopupAlertsData);
-    // $scope.getPopupAlertsData();
-
-
-    $("section.popup-alerts .open").click(function(){
-        $("section.popup-alerts").removeClass('closed');
-        ArtisterilIntervalService.start($scope.closePopupAlerts, 300000, 'closePopupAlerts', true);
-    });
-    $("section.popup-alerts .close").click(function(){
-        $scope.closePopupAlerts();
-    });
-
-    $scope.closePopupAlerts = function() {
-        $("section.popup-alerts").addClass('closed');
-        ArtisterilIntervalService.stop('closePopupAlerts');
-    }
-
-
-
 
 
 	/* mapa *********************************************************************************/

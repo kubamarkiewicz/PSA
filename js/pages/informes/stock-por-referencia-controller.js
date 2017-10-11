e2850e9e2976d4d8bec9ab03a07c691cc6217a5e
Kuba Markiewicz <kuba.markiewicz@gmail.com>
2017-10-11 18:28:10 +0200

    informe stock por referencia

--- js/pages/informes/stock-por-referencia-controller.js
+++ js/pages/informes/stock-por-referencia-controller.js
@@ -1,8 +1,65 @@
-
-
-app.controller('StockPorReferenciaController', function($scope, $rootScope, $http, $routeParams, config, ArtisterilIntervalService, $mdToast) {  
+app.controller('StockPorReferenciaController', function($scope, $rootScope, $http, $routeParams, config, ArtisterilIntervalService, $mdToast, exportUiGridService) {  
 
 	$scope.selectedIndex = 1;
+
+	$scope.ref = '';
+	$scope.StockPiezas = '';
+	$scope.StockPallets = '';
+	$scope.MinPiezas = '';
+	$scope.MaxPiezas = '';
+	$scope.CMJ = '';
+
+    $scope.loadData = function()
+    {
+	    $http({
+	        method  : 'GET',
+	        url     : config.webservice.urls.informes_get_stock_por_referencia,
+            params  : {
+                "Ref" : $scope.ref
+            }
+	    })
+	    .then(function(response) {
+	        $scope.StockPiezas = response.data.get_stock_por_referenciaResult.StockPiezas;
+	        $scope.StockPallets = response.data.get_stock_por_referenciaResult.StockPallets;
+	        $scope.MinPiezas = response.data.get_stock_por_referenciaResult.MinPiezas;
+	        $scope.MaxPiezas = response.data.get_stock_por_referenciaResult.MaxPiezas;
+	        $scope.CMJ = response.data.get_stock_por_referenciaResult.CMJ;
+	        
+	        $scope.gridOptions.data = response.data.get_stock_por_referenciaResult.Posiciones;
+	    });
+    }
+    // $scope.loadData();
+
+
+    $scope.gridOptions = { 
+        columnDefs: [
+            {field: 'Pasillo', type: 'numberStr'},
+            {field: 'Alveolo', type: 'numberStr'},
+            {field: 'Altura', type: 'numberStr'}
+        ],
+        enableRowSelection: false, 
+        enableRowHeaderSelection: false, 
+        multiSelect: false, 
+        modifierKeysToMultiSelect: false
+    };
+
+    $scope.gridOptions.onRegisterApi = function(gridApi)
+    {
+        //set gridApi on scope
+        $scope.gridApi = gridApi;
+    };
+
+
+    setTimeout(function(){ 
+        $('form.myForm input:text').first().focus();
+    }, 0);
+    
+
+
+    $scope.exportExcel = function() 
+    {
+        exportUiGridService.exportToExcel('sheet 1', $scope.gridApi, 'all', 'all');
+    };
 
 
 });
